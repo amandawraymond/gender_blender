82a22895df757a4c4afe636f67201dca2810e571
amandawraymond <amandawraymond@gmail.com>
2015-01-26 03:56:25 -0500

    add links

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do  resources :trainers
   match '/correct'   , to: 'contestants#correct'  , via: 'get'
   match '/incorrect' , to: 'contestants#incorrect', via: 'get'
   
-  root 'trainers#index'
+  root 'contestants#new'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
